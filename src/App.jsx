3a4ea81196dd2b484bc756a4a8ef247b91f0b100
Prabeen <prabingiri922@gmail.com>
2024-01-05 18:12:20 -0600

    added about

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React,{ useState } from 'react';
+import React from 'react';
 import styles from "./styles.module.css";
 import {Navbar} from "./components/Navbar/Navbar";
 import { MainPage } from './components/MainPage/MainPage';
 import { About } from './components/About/About';
 
 function App() {
-  //const [count, setCount] = useState(0)
   return (
     <div className={styles.container}>
       <Navbar/>
       <MainPage/>
+      <About/>
     </div>
   )
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,8 +4,43 @@ import styles from "./about.module.css";
 
 export const About =()=>{
     return(
-        <section>
-            <h3>About</h3>
+        <section className={styles.container} id="about">
+            <h3 className={styles.title}>About</h3>
+            <div className={styles.contentBox}>
+            <img className={styles.img2} src={getUrl("about/img2.png")} alt="about img"/>
+            <ul className={styles.content}>
+          <li className={styles.list}>
+            <img src={getUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.contentText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.list}>
+            <img src={getUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.contentText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.list}>
+            <img src={getUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.contentText}>
+              <h3>Student</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li>
+        </ul>
+            </div>
         </section>
     )
 }
