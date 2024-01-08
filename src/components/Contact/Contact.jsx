f893dcaf23f5a655b04c62d1b1b84f078bed91b6
Prabeen <prabingiri922@gmail.com>
2024-01-07 22:02:45 -0600

    contact complete

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,22 +3,53 @@ import styles from "./contact.module.css";
 
 export const Contact = () => {
   return (
-    <section>
-      <h3>Contact</h3>
-      <a href="mailto:prabingiri922@gmail.com">prabingiri922@gmail.com</a>
-      <div className={styles.socialIcons}>
-        <a>
-          <i className="fa-brands fa-linkedin"></i>
+    <section className={styles.container}>
+      <div className={styles.leftContainer}>
+        <h3 className={styles.title}>Contact</h3>
+        <a className={styles.gmail} href="mailto:prabingiri922@gmail.com">
+          prabingiri922@gmail.com
         </a>
-        <a>
-          <i className="fa-brands fa-github"></i>
-        </a>
-        <a>
-          <i className="fa-brands fa-instagram"></i>
-        </a>
-        <a>
-          <i className="fa-brands fa-twitter"></i>
-        </a>
+        <div className={styles.socialIcons}>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/prabeen-giri-2a8403203/"
+          >
+            <i id={styles.linkedin} className="fa-brands fa-linkedin-in"></i>
+          </a>
+          <a target="_blank" href="https://github.com/prabeen6260">
+            <i id={styles.github} className="fa-brands fa-github"></i>
+          </a>
+          <a target="_blank" href="https://www.instagram.com/prabingiri92/">
+            <i id={styles.instagram} className="fa-brands fa-instagram"></i>
+          </a>
+          <a target="_blank" href="https://twitter.com/prabeen_giri92">
+            <i id={styles.twitter} className="fa-brands fa-x-twitter"></i>
+          </a>
+        </div>
+      </div>
+      <div className={styles.rightContainer}>
+        <form className={styles.formBox}>
+          <input
+            className={styles.name}
+            type="text"
+            placeholder="Your Name"
+            required
+          ></input>
+          <input
+            className={styles.email}
+            type="email"
+            placeholder="Your Email"
+            required
+          ></input>
+          <textarea
+            className={styles.textBox}
+            placeholder="Your Message...."
+            required
+          ></textarea>
+          <button className={styles.submitBtn} type="submit" onClick={() => {}}>
+            Submit
+          </button>
+        </form>
       </div>
     </section>
   );
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,21 +2,43 @@ import React from "react";
 import { getUrl } from "../../utils";
 import styles from "./mainPage.module.css";
 
-export const MainPage=()=>{
-    return(
-        <section className={styles.container}>
-            <div className={styles.description}>
-            <h2 className={styles.title}>Hello! I am Prabeen</h2>
-            <p className={styles.text}> I'm a full-stack developer and a sophmore
-                computer science student.
-            </p>
-            <a target="_blank" href="https://drive.google.com/file/d/19JzJ01Koby9OFpt_83Do1HkOTC5YM1zX/view?usp=sharing" className={styles.resume}>
-                Download Resume
-           </a>
-            </div>
-            <img className={styles.img1} src={getUrl("main/img1.png")}/>
-            <div className={styles.blurOne}/>
-            <div className={styles.blurTwo}/>
-        </section>
-    )
-}
+export const MainPage = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.description}>
+        <h2 className={styles.title}>Hello! I am Prabeen</h2>
+        <p className={styles.text}>
+          {" "}
+          I'm a full-stack developer and a sophmore computer science student.
+        </p>
+        <div className={styles.socialIcons}>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/prabeen-giri-2a8403203/"
+          >
+            <i id={styles.linkedin} className="fa-brands fa-linkedin-in"></i>
+          </a>
+          <a target="_blank" href="https://github.com/prabeen6260">
+            <i id={styles.github} className="fa-brands fa-github"></i>
+          </a>
+          <a target="_blank" href="https://www.instagram.com/prabingiri92/">
+            <i id={styles.instagram} className="fa-brands fa-instagram"></i>
+          </a>
+          <a target="_blank" href="https://twitter.com/prabeen_giri92">
+            <i id={styles.twitter} className="fa-brands fa-x-twitter"></i>
+          </a>
+        </div>
+        <a
+          target="_blank"
+          href="https://drive.google.com/file/d/19JzJ01Koby9OFpt_83Do1HkOTC5YM1zX/view?usp=sharing"
+          className={styles.resume}
+        >
+          Download Resume
+        </a>
+      </div>
+      <img className={styles.img1} src={getUrl("main/img1.png")} />
+      <div className={styles.blurOne} />
+      <div className={styles.blurTwo} />
+    </section>
+  );
+};
